bdc695e2dfbe0cbfd4c967cb4b304f1ecae19735
Coolguy11235 <122454637+Coolguy11235@users.noreply.github.com>
2023-05-24 14:24:31 +0200

    Update Main.java

--- src/Paket/Main.java
+++ src/Paket/Main.java
@@ -10,28 +10,28 @@ import java.util.Comparator;
 public class Main {
 
 	//Mina variabler
-	Scanner scan; //Skapar en Scanner objekt för att kunna ta emot input från användaren
-	int choice; //En integer för att hålla koll på användarens val
-	String[] plats; //En array som ska innehålla listan med bokade platser
+	Scanner scan; //Skapar en Scanner objekt fÃ¶r att kunna ta emot input frÃ¥n anvÃ¤ndaren
+	int choice; //En integer fÃ¶r att hÃ¥lla koll pÃ¥ anvÃ¤ndarens val
+	String[] plats; //En array som ska innehÃ¥lla listan med bokade platser
 	
 	public static void main(String[] args) {
 
-		//Förberedelse för programmets struktur
+		//FÃ¶rberedelse fÃ¶r programmets struktur
 		Main boksys;
 		boksys = new Main();
 				
 		boksys.Setup(); //Initialiserar listan med bokade platser
-		boksys.Menu(); //Öppnar huvudmenyn
+		boksys.Menu(); //Ã–ppnar huvudmenyn
 		boksys.Bokning(); //Hanterar bokning
 		boksys.delBokning(); //Tar bort passageraren
-		boksys.Antal_plats(); //Räknar antalet lediga platser
-		boksys.Vinst(); //Beräknar vinsten
-		boksys.Kund_list(); //Visar en lista på kunder
-		boksys.sortKund_list(); //Visar en sorterad lista på kunder
+		boksys.Antal_plats(); //RÃ¤knar antalet lediga platser
+		boksys.Vinst(); //BerÃ¤knar vinsten
+		boksys.Kund_list(); //Visar en lista pÃ¥ kunder
+		boksys.sortKund_list(); //Visar en sorterad lista pÃ¥ kunder
 		
 	}
 
-    // Förberedelse innan programmet börjar
+    // FÃ¶rberedelse innan programmet bÃ¶rjar
     public void Setup() {
     	
         // Reserverade platser
@@ -65,23 +65,23 @@ public class Main {
 			
 		while (true) {
 			System.out.println("\n-------------------------------------------------\n");
-			System.out.println("Bokningssystem för bussresa");
-			System.out.println("\n(Skriv siffran för att välja följande alternativer)\n");
+			System.out.println("Bokningssystem fÃ¶r bussresa");
+			System.out.println("\n(Skriv siffran fÃ¶r att vÃ¤lja fÃ¶ljande alternativer)\n");
 			
 			System.out.println("1. Boka en obokad plats");
 			System.out.println("2. Ta bort en bokad plats");
 			System.out.println("3. Lediga platser");
-			System.out.println("4. Vinsten av antalet sålda biljetter");
+			System.out.println("4. Vinsten av antalet sÃ¥lda biljetter");
 			System.out.println("5. Se kundlistan");
-			System.out.println("6. Se sorterad efter födelsedatum kundlistan");
+			System.out.println("6. Se sorterad efter fÃ¶delsedatum kundlistan");
 			System.out.println("7. Avsluta programmet\n");
 			
-			//Låter dig att skriva vilket alternativ du vill att välja
+			//LÃ¥ter dig att skriva vilket alternativ du vill att vÃ¤lja
 			scan = new Scanner(System.in);
 	        choice = scan.nextInt(); 
 	        scan.nextLine(); 
 			
-	        //Switch-sats som hanterar användarens val i menyn
+	        //Switch-sats som hanterar anvÃ¤ndarens val i menyn
 	        //Case anropar metoder
 			switch (choice) {
 			    case 1: Bokning(); 
@@ -105,24 +105,24 @@ public class Main {
 			    case 7: System.exit(choice); //Avsluta programmet
 			    	break;
 			    
-			    default: System.out.println("Fel siffra skriv en gång till!"); //Visa meddelandet om användaren matar in ett ogiltigt siffra
+			    default: System.out.println("Fel siffra skriv en gÃ¥ng till!"); //Visa meddelandet om anvÃ¤ndaren matar in ett ogiltigt siffra
 			    	break;
 			}	
 		}
 	}
 	
 	
-	//Metod för att boka en plats
+	//Metod fÃ¶r att boka en plats
 	public void Bokning() {
-	    System.out.print("Ange förnamn, efternamn och födelsedatum (yyyymmdd) för att boka plats: ");
+	    System.out.print("Ange fÃ¶rnamn, efternamn och fÃ¶delsedatum (yyyymmdd) fÃ¶r att boka plats: ");
 	    String input = scan.nextLine();
 	    boolean platsBokad = false;
 	    
-	    //Loopa igenom listan med platser för att hitta en ledig plats att boka
+	    //Loopa igenom listan med platser fÃ¶r att hitta en ledig plats att boka
 	    for (int i = 0; i < plats.length; i++) {
 	        if (plats[i] == null) {
 	            plats[i] = input;
-	            System.out.println("Plats " + (i + 1) + " har bokats åt " + input + ".");
+	            System.out.println("Plats " + (i + 1) + " har bokats Ã¥t " + input + ".");
 	            platsBokad = true;
 	            break;
 	        }
@@ -130,30 +130,30 @@ public class Main {
 	    
 	    //Visa meddelandet om det inte finns lediga platser
 	    if (!platsBokad) {
-	        System.out.println("Tyvärr finns det ingen ledig plats");
+	        System.out.println("TyvÃ¤rr finns det ingen ledig plats");
 	    } 
 	    Menu();	
 	}
 	
 	
-    //Metod för att ta bort en bokning
+    //Metod fÃ¶r att ta bort en bokning
 	public void delBokning() {
 		System.out.println("PRESS ONE TIME ENTER BEFORE WRITING!");
-	    System.out.print("Ange passagerarens förnamn, efternamn eller födelsedatum (yyyymmdd) för att ta bort: ");
+	    System.out.print("Ange passagerarens fÃ¶rnamn, efternamn eller fÃ¶delsedatum (yyyymmdd) fÃ¶r att ta bort: ");
 	    scan.nextLine();
 	    String detaljer = scan.nextLine();
 	    boolean pasHittad = false;
 	    
-	    //Loopa igenom listan med platser för att hitta och ta bort passageraren
+	    //Loopa igenom listan med platser fÃ¶r att hitta och ta bort passageraren
 	    for (int i = 0; i < plats.length; i++) {
 	        if (plats[i] != null && plats[i].toLowerCase().contains(detaljer.toLowerCase())) {
-	            System.out.println("Passageraren "+ plats[i] +" har blivit borttagen från plats " + (i + 1));
+	            System.out.println("Passageraren "+ plats[i] +" har blivit borttagen frÃ¥n plats " + (i + 1));
 	            plats[i] = null;
 	            pasHittad = true;
 	        }
 	    }
 	    
-	    //Visa meddelandet om det inte hittas någon passagerare
+	    //Visa meddelandet om det inte hittas nÃ¥gon passagerare
 	    if (!pasHittad) {
 	        System.out.println("Passagerare ej hittad.");
 	    }
@@ -161,7 +161,7 @@ public class Main {
 	}
 
 
-	//Metod för att räkna lediga platser
+	//Metod fÃ¶r att rÃ¤kna lediga platser
     public void Antal_plats() {
     	int ledigaPlatser = 0;
         for (int i = 0; i < plats.length; i++) {
@@ -174,53 +174,51 @@ public class Main {
 	}
     
     
-  //Metod för att beräkna total vinst och skriva ut den
-    public void Vinst() {
-        double totalVinst = vinstBeräkning(plats, 0);
-        System.out.println("Total vinst: " + totalVinst + " kr");
-        Menu();
-    }
+   //Metod fÃ¶r att berÃ¤kna total vinst och skriva ut den
+   public void Vinst() {
+       double totalVinst = vinstBerÃ¤kning(plats);
+       System.out.println("Total vinst: " + totalVinst + " kr");
+       Menu();
+   }
 
-    //Hjälpmetod för att beräkna vinsten
-    private double vinstBeräkning(String[] plats, int index) {
-        if (index >= plats.length) {
-            return 0.0;
+//HjÃ¤lpmetod fÃ¶r att berÃ¤kna vinsten
+private double vinstBerÃ¤kning(String[] plats) {
+    double vinst = 0.0;
+    for (String passagerare : plats) {
+        if (passagerare != null) {
+            double biljettPris = prisBerÃ¤kning(passagerare);
+            vinst += biljettPris;
         }
-        String passagerare = plats[index];
-        if (passagerare == null) {
-            return 0.0; // return 0 kr if passenger has left
-        }
-        double biljettPris = prisBeräkning(passagerare);
-        double vinst = biljettPris + vinstBeräkning(plats, index + 1);
-        return vinst;
     }
+    return vinst;
+}
 
-    //Hjälpmetod för att beräkna biljettpris utifrån ålder
-    private double prisBeräkning(String passagerare) {
+    //HjÃ¤lpmetod fÃ¶r att berÃ¤kna biljettpris utifrÃ¥n Ã¥lder
+    private double prisBerÃ¤kning(String passagerare) {
         String[] parts = passagerare.split(" ");
-        String StrfödelseDatum = parts[2];
-        int födelseDatum = Integer.parseInt(StrfödelseDatum);
-        int idag = Integer.parseInt(fåIdag());
-        int ålder = (idag - födelseDatum) / 10000;
+        String StrfÃ¶delseDatum = parts[2];
+        int fÃ¶delseDatum = Integer.parseInt(StrfÃ¶delseDatum);
+        int idag = Integer.parseInt(fÃ¥Idag());
+        int Ã¥lder = (idag - fÃ¶delseDatum) / 10000;
         // Add year 2023 to make the age system work
-        if (ålder < 18 && födelseDatum <= 20230000) {
+        if (Ã¥lder < 18 && fÃ¶delseDatum <= 20230000) {
             return 149.90;
-        } else if (ålder > 69 || födelseDatum <= 19000000) {
+        } else if (Ã¥lder > 69 || fÃ¶delseDatum <= 19000000) {
             return 200.0;
         } else {
             return 299.90;
         }
     }
 
-    //Hjälpmetod för att få dagens datum som en sträng
-    private String fåIdag() {
+    //HjÃ¤lpmetod fÃ¶r att fÃ¥ dagens datum som en strÃ¤ng
+    private String fÃ¥Idag() {
         DateFormat datumFormat = new SimpleDateFormat("yyyymmdd");
         Date datum = new Date();
         return datumFormat.format(datum);
     }
     
     
-    //Metod för att skriva ut en lista på kunder
+    //Metod fÃ¶r att skriva ut en lista pÃ¥ kunder
     public void Kund_list() {
         System.out.println("\n-------------------------------------------------");
         System.out.println("Kundlista:");
@@ -231,13 +229,13 @@ public class Main {
     }
     
     
-    // Metod för att sortera kundlistan efter födelsedatum och skriva ut den
+    // Metod fÃ¶r att sortera kundlistan efter fÃ¶delsedatum och skriva ut den
     public void sortKund_list() {
         String[] nonNullPlats = Arrays.stream(plats)
                                       .filter(p -> p != null)
                                       .toArray(String[]::new);
         Arrays.sort(nonNullPlats, Comparator.comparing(p -> p.substring(p.length() - 8)));
-        System.out.println("\nKundlista sorterad efter födelsedatum:");
+        System.out.println("\nKundlista sorterad efter fÃ¶delsedatum:");
         
         for (String p : nonNullPlats) {
             System.out.println(p);
